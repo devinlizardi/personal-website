2153d4632fdc0339216e32143ba9b254fbb1e0ba
Devin Lizardi <dlizardi@berkeley.edu>
2021-02-05 15:35:02 -0800

    basic routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './components.css';
 
 function Header() {
-    return(
-        <div className="header">
-            <h1>Devin Lizardi</h1>
-            <p>creative, performer, covered in cat hair</p>
-        </div>
-    );
+	return(
+		<div className="header">
+			<h1>Devin Lizardi</h1>
+			<p>creative, performer, covered in cat hair</p>
+		</div>
+	);
 }
 
 export default Header;
--- src/components/Sidenav.js
+++ src/components/Sidenav.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link
+  } from "react-router-dom";
 import './components.css';
 import coffecup from './../img/coffeecup.jpg';
 
 function Sidenav() {
     return(
-        <img className="coffee-img" src={coffecup} alt="bg image sorry"/>
-    );
+		<>
+		<Router>
+		<div className="content-container">
+			<div className="sidenav-main">
+				<Link to="/"><h2 className="sidenav-title">About</h2></Link>
+				<Link to="/projects"><h2 className="sidenav-title">Projects</h2></Link>
+				<Link to="/contact"><h2 className="sidenav-title">Contact</h2></Link>
+			</div>
+
+			{/* ROUTES */}
+
+			<Switch className="content-main">
+				<Route path="/">
+					<h1>about</h1>
+				</Route>
+				<Route path="/projects">
+					<h1>projects</h1>
+				</Route>
+				<Route path="/contact">
+					<h1>contact</h1>
+				</Route>
+			</Switch>
+		</div>
+		</Router>
+		<img className="coffee-img" src={coffecup} alt="bg image sorry"/>
+		</>
+	);
 }
 
 export default Sidenav;
