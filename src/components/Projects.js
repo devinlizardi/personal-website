1aa536f686b632c6f2e42a07dbdc3608ecc512ca
Devin Lizardi <dlizardi@berkeley.edu>
2021-02-09 23:30:22 -0800

    beginning illustration section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -29,6 +29,23 @@ function About() {
             </div>
             <div className="project-container-secondary">
                 <div className="desc-container" id="reel-text">
+                        <h2>Illustration Progress</h2>
+                        <em>2019-Present</em>
+                        <p>
+                            Aliquet phasellus eu mauris in augue 
+                            tortor in quam in. Vel at id faucibus eget. 
+                            <br /><br />
+                            Diam elementum risus et pretium 
+                            interdum arcu risus. Gravida nisi, lobortis facilisis sed eu vitae. 
+                            Praesent enim, in nam pretium, accumsan accumsan.
+                        </p>
+                    </div>
+                    <div className="image-container">
+                        
+                    </div>
+            </div>
+            <div className="project-container-tertiary">
+                <div className="desc-container" id="reel-text">
                         <h2>Video Reel</h2>
                         <em>2018-2019</em>
                         <p>
