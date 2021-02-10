c57495baa5c0b7f53215476e94bd6ae79513ffa6
Devin Lizardi <dlizardi@berkeley.edu>
2021-02-09 23:55:47 -0800

    reorganize projects

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -27,8 +27,8 @@ function About() {
                     <div id="gif-bg"/>
                 </div>
             </div>
-            <div className="project-container-secondary">
-                <div className="desc-container" id="reel-text">
+            <div className="project-container-secondary changer">
+                <div className="desc-container">
                         <h2>Illustration Progress</h2>
                         <em>2019-Present</em>
                         <p>
@@ -44,24 +44,24 @@ function About() {
                         
                     </div>
             </div>
-            <div className="project-container-tertiary">
-                <div className="desc-container" id="reel-text">
-                        <h2>Video Reel</h2>
-                        <em>2018-2019</em>
-                        <p>
-                            Aliquet phasellus eu mauris in augue 
-                            tortor in quam in. Vel at id faucibus eget. 
-                            <br /><br />
-                            Diam elementum risus et pretium 
-                            interdum arcu risus. Gravida nisi, lobortis facilisis sed eu vitae. 
-                            Praesent enim, in nam pretium, accumsan accumsan.
-                        </p>
-                    </div>
-                    <div className="image-container" id="vimeo-iframe">
-                        <iframe src="https://player.vimeo.com/video/346242259" width="640" height="360" 
-                            frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" 
-                            allowFullScreen></iframe>
-                    </div>
+            <div className="project-container-tertiary changer">
+                <div className="desc-container">
+                    <h2>Video Reel</h2>
+                    <em>2018-2019</em>
+                    <p id="reel-text">
+                        Aliquet phasellus eu mauris in augue 
+                        tortor in quam in. Vel at id faucibus eget. 
+                        <br /><br />
+                        Diam elementum risus et pretium 
+                        interdum arcu risus. Gravida nisi, lobortis facilisis sed eu vitae. 
+                        Praesent enim, in nam pretium, accumsan accumsan.
+                    </p>
+                </div>
+                <div className="image-container" id="vimeo-iframe">
+                    <iframe src="https://player.vimeo.com/video/346242259" width="640" height="360" 
+                        frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" 
+                        allowFullScreen></iframe>
+                </div>
             </div>
         </div>
     );
