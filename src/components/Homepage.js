030a98b991272e1e8c6f128c483b753c7d2117ac
Devin Lizardi <dlizardi@berkeley.edu>
2021-03-09 20:34:21 -0800

    temp homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,29 +1,14 @@
 import React from 'react';
 import './components.css';
 
-import portrait from'./../img/IMG_2498.JPG';
-
 function About() {
     return(
-        <div className="content-main">
-            <img id="portrait" src={portrait} alt="Me and my cat! Finn is a 
-                3 year old very very soft brown cat who likes to
-                play hide-and-seek-cuddles"/>
-            <div id="about-text">
-                <h1>Hello, I&#39;m the homepage</h1>
-                <p>
-                    Dui aliquet vestibulum sit augue fringilla. Ultricies ante non tortor 
-                    aliquam diam interdum dolor. Mauris lacus, malesuada euismod et arcu. 
-                    In consequat turpis semper sed a. Aliquet phasellus eu mauris in augue 
-                    tortor in quam in. Vel at id faucibus eget. 
-                    <br /><br />
-                    Diam elementum risus et pretium 
-                    interdum arcu risus. Gravida nisi, lobortis facilisis sed eu vitae. 
-                    Praesent enim, in nam pretium, accumsan accumsan.
-                    <br /><br />
-                    This website was developed by me! View the source code <a> at this repository</a>
-                </p>
-            </div>
+        <div className="homepage-main">
+            <p id="spinner">devin is still working
+            <br /> on the homepage it will be 
+            <br /> so cool and mindblowing you have no idea
+            <br /> just click on something else for now
+            </p>
         </div>
     );
 }
